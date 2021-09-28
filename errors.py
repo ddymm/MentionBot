6367d38dc28bd3c0f30082cb9fa85063573c763e
Dmitry <dm3try@gmail.com>
2021-09-28 18:09:00 +0500

    Поправил тайпинг

--- errors.py
+++ errors.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, Text
-
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 KNOWN_API_ERRORS: Dict = {
     "ratelimited": "Превышен лимит запросов к API :("
@@ -12,9 +12,9 @@ class ApiError(Exception):
     message: Text = "Неизвестная ошибка API :("
     error: Text = None
 
-    def __init__(self, api_error):
+    def __init__(self, api_error: SlackApiError):
         self.error = api_error.response["error"]
         self.message = KNOWN_API_ERRORS.get(self.error, self.message)
 
-    def handle(self, client, data):
+    def handle(self, client: WebClient, data: Dict):
         client.chat_postMessage(text=self.message, **data)
